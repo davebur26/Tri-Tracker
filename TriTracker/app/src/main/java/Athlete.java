c4de7fb43b6b2d13716152bd8164c38494d94aeb
davebur26 <davebur26@gmail.com>
2017-09-22 17:27:53 +0100

    add Athlete constructor

--- TriTracker/app/src/main/java/Athlete.java
+++ TriTracker/app/src/main/java/Athlete.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by davidburnett on 22/09/2017.
  */
 
 public class Athlete {
+
+    String name;
+    ArrayList<Activity>disciplineLog;
+    HashMap<Discipline, disciplineLog>activityLog;
+
+    public Athlete (String name){
+        this.name = name;
+        disciplineLog = new ArrayList<>();
+        activityLog = new HashMap<>();
+    }
 }
